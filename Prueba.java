dc9719515428e2864e64c90ace407b199c80d329
Diego <didirey27@gmail.com>
2024-10-03 12:33:33 +0200

    QuitarApellidos_GrupoMusica

--- Prueba.java
+++ Prueba.java
@@ -2,9 +2,9 @@
 	
 	public static void main(String args[]) {
 		
-		System.out.println("Diez Rey");
 		System.out.println("18 años 2006");
 		System.out.println("Pasta");
+		System.out.println("The Beatles");
 	}
 
 }
